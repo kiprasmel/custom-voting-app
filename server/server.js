b669b5bd130d803c7e009c599845a6bdb1d15d4f
Kipras Melnikovas <sarpixo@gmail.com>
2018-11-14 10:37:02 +0200

    Update the server.js root file
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
  */
 
 /** load .env environment variables to process.env */
+"use strict";
 require("./utils/loadDotenv")("../.env");
 
 const express = require("express");
@@ -15,7 +16,7 @@ const helmet = require("helmet");
 /// foo(bar)
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT | 5000;
 
 app.use(helmet()); // https://helmetjs.github.io/
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +26,15 @@ app.use(express.json());
 require("./utils/connectToMongoDB")();
 
 /** Use routes: */
+app.use("/api/poll", require("./routes/poll"));
 app.use("/api/vote", require("./routes/vote"));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(port, () => console.log(`~Server started on port ${port}`));
