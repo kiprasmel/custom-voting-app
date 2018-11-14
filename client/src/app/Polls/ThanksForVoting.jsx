5e11898959e271231313afa0f47a2124a8e97ff6
Kipras Melnikovas <sarpixo@gmail.com>
2018-11-14 10:47:14 +0200

    Create ThanksForVoting Component
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ client/src/app/Polls/ThanksForVoting.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+
+class ThanksForVoting extends Component {
+	// static propTypes = {};
+
+	render() {
+		// const { pollName } = this.props.match.params;
+
+		return (
+			<>
+				<h1 className="display-4 mb-4 text-xblue">Dėkojame už balsavimą!</h1>
+				<h3 className="text-xlight">
+					Tavo balsas sėkmingai įrašytas. Gali ramiai laukti rezultatų!
+				</h3>
+			</>
+		);
+	}
+}
+
+export default ThanksForVoting;
