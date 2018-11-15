428f751f924061fdc853772e3a58c86d44109383
Kipras Melnikovas <sarpixo@gmail.com>
2018-11-16 00:00:19 +0200

    Update Landing - add .toLowerCase() for code input
    When searching for polls by code, it currently must be done using only lower case letters.
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- client/src/app/RootPage/Landing.jsx
+++ client/src/app/RootPage/Landing.jsx
@@ -18,7 +18,7 @@ class Landing extends Component {
 
 	handleChange = (e) => {
 		this.setState({
-			[e.target.name]: e.target.value,
+			[e.target.name]: e.target.value.toLowerCase(),
 		});
 	};
 
